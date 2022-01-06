aa67edca8101a16f08701d757c421a5429eec2d0
filsMampuyaKinDb <fils.mampuya@kindistribution.com>
2022-01-06 14:22:54 +0100

    voir si possible

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ const functions = require("firebase-functions");
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  console.log('voir si possible');
+  console.log('voir si possible ');
   functions.logger.info("Bonjour lllog!  lajoie", {structuredData: true});
   response.send("Hello from Firebase ok 12 !");
 });
