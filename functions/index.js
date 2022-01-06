8597148353c19afe3e838439d9dd1f8bbc105caa
filsMampuyaKinDb <fils.mampuya@kindistribution.com>
2022-01-06 10:32:55 +0100

    hello work

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,6 @@ const functions = require("firebase-functions");
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello log !", {structuredData: true});
+  functions.logger.info("Hello log ok!", {structuredData: true});
   response.send("Hello from Firebase fils !");
 });
